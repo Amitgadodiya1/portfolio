3e8fc5953fab8154685ec3cd90e7f04f85536df1
Amitgadodiya1 <amitgadodiya12@gmail.com>
2023-02-20 05:00:21 +0530

    Resume Updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Cv from '../../assets/AmitGadodiya.docx'
+import React from "react";
+import Cv from "../../assets/AmitGadodiyaResume.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={Cv} className="btn"> Download CV</a>
-      <a href="#contact"  className="btn btn-primary" > Lets Talk</a>
+      <a href={Cv} className="btn">
+        {" "}
+        Download CV
+      </a>
+      <a href="#contact" className="btn btn-primary">
+        {" "}
+        Lets Talk
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
